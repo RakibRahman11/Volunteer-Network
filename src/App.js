171eea677c6c3f53a79e8ce5a3996b0279a3a9db
RakibRahman11 <rahmanrakib207@gmail.com>
2021-10-27 17:45:36 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Home/Footer/Footer';
+import Home from './components/Home/Home/Home';
+import NavBar from './components/Home/NavBar/NavBar';
+import PageError from './components/PageError/PageError';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <NavBar></NavBar>
+        <Switch>
+          <Route exact path='/'>
+            <Home></Home>
+          </Route>
+          <Route exact path='/home'>
+            <Home></Home>
+          </Route>
+          <Route path='*'>
+            <PageError></PageError>
+          </Route>
+        </Switch>
+        <Footer></Footer>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Home/NavBar/NavBar.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.css'
+import logo from '../../../images/logos/Group 1329.png'
+
+const NavBar = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light ">
+                <div className="container w-100 d-flex justify-content-between align-content-between">
+                    <div>
+                        <Link className="navbar-brand" to="/home">
+                            <img src={logo} alt="" width="140" height="60" />
+                        </Link>
+                    </div>
+                    <div className="collapse navbar-collapse">
+                        <ul className="mb-2 navbar-nav me-auto mb-lg-0">
+                            <li className="nav-item">
+                                <Link className="nav-link active" to="/home">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link active" to="/">Donation</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link active" to="/">Events</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link active" to="/">Blog</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="btn-text me-2 nav-link btn btn-outline-primary" to="/">Register</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link btn btn-outline-dark" to="/">Admin</Link>
+                            </li>
+                        </ul>
+                        {/* <img className='rounded-circle me-2' src={user.photoURL} alt="" />
+                        {user.displayName}
+                        {isLogin.displayName}
+                        <form className="d-flex">
+                            {
+                                user.displayName ?
+                                    <button onClick={logout} className='btn '>Logout</button> :
+                                    <Link className="navbar-brand" to="/login">
+                                        {user.displayName} <img src={login} alt="" width="30" height="25" />
+                                    </Link>
+                            }
+                        </form> */}
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default NavBar;
